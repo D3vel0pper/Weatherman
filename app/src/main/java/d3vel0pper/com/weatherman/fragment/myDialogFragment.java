7de36b7d5e9c4b7312066cdeeb205fbda23573dc
D3vel0pper <toamaindex@gmail.com>
2016-04-29 11:45:33 +0900

    fix #11 DialogFragment dismiss

--- app/src/main/java/d3vel0pper/com/weatherman/fragment/myDialogFragment.java
+++ app/src/main/java/d3vel0pper/com/weatherman/fragment/myDialogFragment.java
@@ -7,6 +7,7 @@ import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import d3vel0pper.com.weatherman.R;
@@ -16,15 +17,25 @@ import d3vel0pper.com.weatherman.R;
  */
 public class myDialogFragment extends DialogFragment {
     private String dialogData;
+    private myDialogFragment instance;
 
     @Override
     public View onCreateView(LayoutInflater inflater,ViewGroup viewGroup, Bundle bundle){
         super.onCreateView(inflater,viewGroup,bundle);
+        instance = this;
         //Inflating layout of dialog
         View view = inflater.inflate(R.layout.dialog_layout,null);
         //ValuePassing From HttpResponseTask
         TextView textView = (TextView)view.findViewById(R.id.dialogText);
         textView.setText(dialogData);
+        Button closeBtn = (Button)view.findViewById(R.id.clsBtn);
+        closeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                instance.dismiss();
+            }
+        });
+
         return view;
 
 
@@ -34,8 +45,9 @@ public class myDialogFragment extends DialogFragment {
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState){
 
-        Dialog dialog = super.onCreateDialog(savedInstanceState);
+        final Dialog dialog = super.onCreateDialog(savedInstanceState);
         dialog.setTitle("Detail");
+        dialog.setContentView(R.layout.dialog_layout);
 
         return dialog;
     }
